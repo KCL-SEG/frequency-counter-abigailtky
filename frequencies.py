13c6888ff3f818d1ee6b095e3c06858d9486e03a
abigailtky <127849272+abigailtky@users.noreply.github.com>
2023-11-13 15:12:04 +0000

    Fix error of not adding new items to list

--- frequencies.py
+++ frequencies.py
@@ -3,10 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    
     for item in items:
         if not isinstance(item, str):
             item = str(item)
         if item in frequencies:
             frequencies[item] += 1
+        else:
+            frequencies[item] = 1
     return frequencies
