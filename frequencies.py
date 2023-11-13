702c4374cfe123b03e9aeb24ee8a939a22fd8210
abigailtky <127849272+abigailtky@users.noreply.github.com>
2023-11-13 14:59:51 +0000

    Fix error in frequencies function

--- frequencies.py
+++ frequencies.py
@@ -3,8 +3,9 @@
 
 def frequencies(items):
     frequencies = {}
+    
     for item in items:
         if not isinstance(item, str):
-            frequencies[item] = str(item)
+            item = str(item)
         frequencies[item] = items.count(item)
     return frequencies
